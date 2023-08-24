a846f2b11264fc2656a2dbf8e1b2322d31c4d26c
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-08-24 17:17:12 +0300

    fixed Signup

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -34,154 +34,156 @@ export const Signup = props => {
     reset();
   };
 
-  if (isClient !== undefined) {
-    return (
-      <div className="signup">
-        <h1>{title}</h1>
-        <AuthIntegration />
+  return (
+    <>
+      {isClient !== undefined ? (
+        <div className="signup">
+          <h1>{title}</h1>
+          <AuthIntegration />
+          <FormAuth
+            child={
+              <>
+                <label
+                  htmlFor="name"
+                  className="form-auth__label"
+                >
+                  Имя
+                  <input
+                    className=""
+                    type="text"
+                    id="name"
+                    minLength="1"
+                    maxLength="50"
+                    {...register('name', {
+                      required: ERR_MESSAGE_REQUIRED,
+                      pattern: {
+                        value: REG_NAME,
+                        message: ERR_MESSAGE_INVALIDTEXT
+                      }
+                    })}
+                  />
+                  <span className="form-auth__err">{errors?.name && errors.name.message}</span>
+                </label>
+                <label
+                  htmlFor="reg-surname"
+                  className="form-auth__label"
+                >
+                  Фамилия
+                  <input
+                    className=""
+                    type="text"
+                    id="surname"
+                    minLength="1"
+                    maxLength="50"
+                    {...register('surname', {
+                      required: ERR_MESSAGE_REQUIRED,
+                      pattern: {
+                        value: REG_SURNAME,
+                        message: ERR_MESSAGE_INVALIDTEXT
+                      }
+                    })}
+                  />
+                  <span className="form-auth__err">
+                    {errors?.surname && errors.surname.message}
+                  </span>
+                </label>
+                <label
+                  htmlFor="email"
+                  className="form-auth__label"
+                >
+                  Email
+                  <input
+                    className=""
+                    type="email"
+                    id="email"
+                    {...register('email', {
+                      required: ERR_MESSAGE_REQUIRED,
+                      pattern: {
+                        value: REG_EMAIL,
+                        message: ERR_MESSAGE_INVALIDEMAIL
+                      }
+                    })}
+                  />
+                  <span className="form-auth__err">{errors?.email && errors.email.message}</span>
+                </label>
+                <label
+                  htmlFor="password"
+                  className="form-auth__label"
+                >
+                  Пароль
+                  <input
+                    className=""
+                    type="password"
+                    id="password"
+                    minLength="8"
+                    maxLength="50"
+                    {...register('password', {
+                      required: ERR_MESSAGE_REQUIRED,
+                      pattern: {
+                        value: REG_PASSWORD,
+                        message: ERR_MESSAGE_INVALIDPASSWORD
+                      }
+                    })}
+                  />
+                  <span className="form-auth__err">
+                    {errors?.password && errors.password.message}
+                  </span>
+                </label>
+              </>
+            }
+            buttonTitle={'Присоединиться'}
+            onSubmit={handleSubmit(handleSubmitSignup)}
+            isFormValid={isValid}
+          />
+        </div>
+      ) : (
         <FormAuth
           child={
-            <>
-              <label
-                htmlFor="name"
-                className="form-auth__label"
-              >
-                Имя
+            <fieldset className="signup__fieldset">
+              <legend className="signup__title">
+                Присоединитесь
+                <br />
+                как заказчик или специалист
+              </legend>
+
+              <div className="signup__input-container">
                 <input
-                  className=""
-                  type="text"
-                  id="name"
-                  minLength="1"
-                  maxLength="50"
-                  {...register('name', {
-                    required: ERR_MESSAGE_REQUIRED,
-                    pattern: {
-                      value: REG_NAME,
-                      message: ERR_MESSAGE_INVALIDTEXT
-                    }
-                  })}
+                  type="radio"
+                  id="client"
+                  name="type"
+                  value="client"
+                  className="signup__input-radio"
                 />
-                <span className="form-auth__err">{errors?.name && errors.name.message}</span>
-              </label>
-              <label
-                htmlFor="reg-surname"
-                className="form-auth__label"
-              >
-                Фамилия
+                <label
+                  htmlFor="client"
+                  className="signup__input-label"
+                >
+                  Я заказчик
+                </label>
+              </div>
+
+              <div className="signup__input-container">
                 <input
-                  className=""
-                  type="text"
-                  id="surname"
-                  minLength="1"
-                  maxLength="50"
-                  {...register('surname', {
-                    required: ERR_MESSAGE_REQUIRED,
-                    pattern: {
-                      value: REG_SURNAME,
-                      message: ERR_MESSAGE_INVALIDTEXT
-                    }
-                  })}
+                  type="radio"
+                  id="expert"
+                  name="type"
+                  value="expert"
+                  className="signup__input-radio"
                 />
-                <span className="form-auth__err">{errors?.surname && errors.surname.message}</span>
-              </label>
-              <label
-                htmlFor="email"
-                className="form-auth__label"
-              >
-                Email
-                <input
-                  className=""
-                  type="email"
-                  id="email"
-                  {...register('email', {
-                    required: ERR_MESSAGE_REQUIRED,
-                    pattern: {
-                      value: REG_EMAIL,
-                      message: ERR_MESSAGE_INVALIDEMAIL
-                    }
-                  })}
-                />
-                <span className="form-auth__err">{errors?.email && errors.email.message}</span>
-              </label>
-              <label
-                htmlFor="password"
-                className="form-auth__label"
-              >
-                Пароль
-                <input
-                  className=""
-                  type="password"
-                  id="password"
-                  minLength="8"
-                  maxLength="50"
-                  {...register('password', {
-                    required: ERR_MESSAGE_REQUIRED,
-                    pattern: {
-                      value: REG_PASSWORD,
-                      message: ERR_MESSAGE_INVALIDPASSWORD
-                    }
-                  })}
-                />
-                <span className="form-auth__err">
-                  {errors?.password && errors.password.message}
-                </span>
-              </label>
-            </>
+                <label
+                  htmlFor="expert"
+                  className="signup__input-label"
+                >
+                  Я фотограф/видеооператор
+                </label>
+              </div>
+            </fieldset>
           }
           buttonTitle={'Присоединиться'}
-          onSubmit={handleSubmit(handleSubmitSignup)}
+          onSubmit={handleSubmitJoin}
           isFormValid={isValid}
         />
-      </div>
-    );
-  } else {
-    return (
-      <FormAuth
-        child={
-          <fieldset className="signup__fieldset">
-            <legend className="signup__title">
-              Присоединитесь
-              <br />
-              как заказчик или специалист
-            </legend>
-
-            <div className="signup__input-container">
-              <input
-                type="radio"
-                id="client"
-                name="type"
-                value="client"
-                className="signup__input-radio"
-              />
-              <label
-                htmlFor="client"
-                className="signup__input-label"
-              >
-                Я заказчик
-              </label>
-            </div>
-
-            <div className="signup__input-container">
-              <input
-                type="radio"
-                id="expert"
-                name="type"
-                value="expert"
-                className="signup__input-radio"
-              />
-              <label
-                htmlFor="expert"
-                className="signup__input-label"
-              >
-                Я фотограф/видеооператор
-              </label>
-            </div>
-          </fieldset>
-        }
-        buttonTitle={'Присоединиться'}
-        onSubmit={handleSubmitJoin}
-        isFormValid={isValid}
-      />
-    );
-  }
+      )}
+    </>
+  );
 };
