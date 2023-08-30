9560a436ea4abbf3ff6a2cf63e18f9a587810a65
TiFox <mozi.rin@yandex.ru>
2023-08-30 15:44:14 +0300

    refactor: настроить визуал чекбоксов

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -37,46 +37,43 @@ function Filter() {
 
       <fieldset className="fild">
         <p className="filter__category">вид съемки</p>
-        <label className="input">
-          <input type="checkbox" name="type-of-shooting" />
-          Свадебная
-        </label>
-        <label className="input">
-          <input type="checkbox" name="type-of-shooting" />
-          Love Story
-        </label>
-        <label className="input">
-          <input type="checkbox" name="type-of-shooting" />
-          Индивидуальная
-        </label>
-        <label className="input">
-          <input type="checkbox" name="type-of-shooting" />
-          Семейная
-        </label>
-        <label className="input">
-          <input type="checkbox" name="type-of-shooting" />
-          Fashion
-        </label>
-        <label className="input">
-          <input type="checkbox" name="type-of-shooting" />
-          Питомцы
-        </label>
-        <label className="input">
-          <input type="checkbox" name="type-of-shooting" />
-          Интервью
-        </label>
-        <label className="input">
-          <input type="checkbox" name="type-of-shooting" />
-          Аэросъемка
-        </label>
-        <label className="input">
-          <input type="checkbox" name="type-of-shooting" />
-          Стоковая
-        </label>
-        <label className="input">
-          <input type="checkbox" name="type-of-shooting" />
-          Клипы
-        </label>
+        
+          <input type="checkbox" name="type-of-shooting" id="wedding" className="checkbox"/>
+          <label className="checkbox-label" for="wedding">Свадебная</label>
+
+          <input type="checkbox" name="type-of-shooting" id="love-story" className="checkbox"/>
+          <label className="checkbox-label" for="love-story"> Love Story</label>
+       
+          <input type="checkbox" name="type-of-shooting" id="individual" className="checkbox"/>
+          <label className="checkbox-label" for="individual"> Индивидуальная</label>
+        
+          <input type="checkbox" name="type-of-shooting" id="family" className="checkbox"/>
+         <label className="checkbox-label" for="family"> Семейная</label>
+        
+    
+          <input type="checkbox" name="type-of-shooting" id="fashion" className="checkbox"/>
+             <label className="checkbox-label" for="fashion"> Fashion </label>
+       
+        
+          <input type="checkbox" name="type-of-shooting" id="pets" className="checkbox"/>
+          <label className="checkbox-label" for="pets">Питомцы</label>
+        
+        
+          <input type="checkbox" name="type-of-shooting" id="interview" className="checkbox"/>
+         <label className="checkbox-label" for="interview"> Интервью </label>
+       
+        
+          <input type="checkbox" name="type-of-shooting" id="aerial" className="checkbox"/>
+         <label className="checkbox-label" for="aerial"> Аэросъемка </label>
+       
+        
+          <input type="checkbox" name="type-of-shooting" id="stock" className="checkbox"/>
+         <label className="checkbox-label" for="stock"> Стоковая</label>
+        
+        
+          <input type="checkbox" name="type-of-shooting" id="clips" className="checkbox"/>
+         <label className="checkbox-label" for="clips"> Клипы</label>
+        
       </fieldset>
 
   <p className="filter__category">стоимость</p> 
