1569f535db74f574aea1be3cccd1d78db11d71f6
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-08-31 17:27:14 +0300

    add rendering reset-password page

--- src/pages/ResetPassword/ResetPassword.jsx
+++ src/pages/ResetPassword/ResetPassword.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import './ResetPassword.css';
 import { FormAuth } from '../../components/FormAuth/FormAuth';
-import ok_image from '../../images/Ok.svg';
+import ok_image from '../../images/Illustration -Success.svg';
 import {
   REG_EMAIL,
   REG_PASSWORD,
@@ -14,6 +14,24 @@ import {
 
 export const ResetPassword = props => {
   const navigate = useNavigate();
+  const location = useLocation();
+  // new URLSearchParams(location.search).get("uid")
+  // new URLSearchParams(location.search).get("token")
+
+  const [searchParam, setSearchParam] = useState(undefined);
+
+  const returnSearchParam = () => {
+    const param = {
+      uid: new URLSearchParams(location.search).get('uid'),
+      token: new URLSearchParams(location.search).get('token')
+    };
+    return param;
+  };
+
+  useEffect(() => {
+    setSearchParam(returnSearchParam());
+  }, []);
+
   const {
     watch,
     register,
@@ -43,101 +61,117 @@ export const ResetPassword = props => {
     reset();
   };
 
-  return (
-    <>
-      {isEmailSend ? (
-        <div className="reset-password form-auth__container">
-          <img
-            src={ok_image}
-            alt="ok"
-          />
-          <h1 className="form-auth__title">Письмо отправлено</h1>
-          <p>
-            Проверьте свой email и&nbsp;перейдите по&nbsp;ссылке в&nbsp;письме, чтобы продолжить
-            сброс пароля
-          </p>
-        </div>
-      ) : (
-        <div className="reset-password form-auth__container">
-          <h1 className="form-auth__title">Сброс пароля</h1>
-          <h2 className="form-auth__subtitle">
-            Введите email и мы отправим письмо с ссылкой для создания нового пароля
-          </h2>
-          <FormAuth
-            child={
-              <>
-                <label
-                  htmlFor="sendEmail"
-                  className="form-auth__label"
-                >
-                  Email
-                  <input
-                    className={formAuthInputClassName('sendEmail')}
-                    type="email"
-                    id="sendEmail"
-                    {...register('sendEmail', {
-                      required: ERR_MESSAGE_REQUIRED,
-                      pattern: {
-                        value: REG_EMAIL,
-                        message: ERR_MESSAGE_INVALIDEMAIL
-                      }
-                    })}
-                  />
-                  <span className="form-auth__err">
-                    {errors?.sendEmail && errors.sendEmail.message}
-                  </span>
-                </label>
-              </>
-            }
-            buttonTitle={'Отправить письмо'}
-            onSubmit={handleSubmit(handleSubmitSendEmailToResetPassword)}
-            err={errors}
-          />
-          <button
-            className="form-auth__button_sign"
-            onClick={() => navigate('/sign-in')}
-          >
-            Вернуться на страницу входа
-          </button>
-        </div>
-      )}
-      {isPasswordReset ? (
-        <></>
-      ) : (
-        <div className="reset-password form-auth__container">
-          <h1 className="form-auth__title">Сброс пароля</h1>
-          <FormAuth
-            child={
-              <>
-                <label
-                  htmlFor="resetPassword"
-                  className="form-auth__label"
-                >
-                  Новый пароль
-                  <input
-                    className=""
-                    type="password"
-                    id="resetPassword"
-                    {...register('resetPassword', {
-                      required: ERR_MESSAGE_REQUIRED,
-                      pattern: {
-                        value: REG_PASSWORD,
-                        message: ERR_MESSAGE_INVALIDPASSWORD
-                      }
-                    })}
-                  />
-                  <span className="form-auth__err">
-                    {errors?.resetPassword && errors.resetPassword.message}
-                  </span>
-                </label>
-              </>
-            }
-            buttonTitle={'Сбросить пароль'}
-            onSubmit={handleSubmit(handleSubmitResetPassword)}
-            err={errors}
-          />
-        </div>
-      )}
-    </>
-  );
+  if (searchParam?.uid === null && searchParam?.token === null && isEmailSend) {
+    return (
+      <div className="form-auth__container">
+        <img
+          src={ok_image}
+          alt="ok"
+          className="reset-password__image"
+        />
+        <h1 className="form-auth__title">Письмо отправлено</h1>
+        <p className="form-auth__subtitle">
+          Перейдите по ссылке из письма и откроется окно для создания нового пароля
+        </p>
+      </div>
+    );
+  } else if (searchParam?.uid === null && searchParam?.token === null && !isEmailSend) {
+    return (
+      <div className="form-auth__container">
+        <h1 className="form-auth__title">Сброс пароля</h1>
+        <h2 className="form-auth__subtitle">
+          Введите email и мы отправим письмо с ссылкой для создания нового пароля
+        </h2>
+        <FormAuth
+          child={
+            <>
+              <label
+                htmlFor="sendEmail"
+                className="form-auth__label"
+              >
+                Email
+                <input
+                  className={formAuthInputClassName('sendEmail')}
+                  type="email"
+                  id="sendEmail"
+                  {...register('sendEmail', {
+                    required: ERR_MESSAGE_REQUIRED,
+                    pattern: {
+                      value: REG_EMAIL,
+                      message: ERR_MESSAGE_INVALIDEMAIL
+                    }
+                  })}
+                />
+                <span className="form-auth__err">
+                  {errors?.sendEmail && errors.sendEmail.message}
+                </span>
+              </label>
+            </>
+          }
+          buttonTitle={'Отправить письмо'}
+          onSubmit={handleSubmit(handleSubmitSendEmailToResetPassword)}
+          err={errors}
+        />
+        <button
+          className="form-auth__button_sign"
+          onClick={() => navigate('/sign-in')}
+        >
+          Вернуться на страницу входа
+        </button>
+      </div>
+    );
+  } else if (searchParam?.uid !== null && searchParam?.token !== null && isPasswordReset) {
+    return (
+      <div className="form-auth__container">
+        <img
+          src={ok_image}
+          alt="ok"
+          className="reset-password__image"
+        />
+        <h1 className="form-auth__title">Пароль сохранен</h1>
+        <button
+          className="form-auth__button_submit"
+          onClick={() => navigate('/sign-in')}
+        >
+          Войти в аккаунт
+        </button>
+      </div>
+    );
+  } else if (searchParam?.uid !== null && searchParam?.token !== null && !isPasswordReset) {
+    return (
+      <div className="form-auth__container">
+        <h1 className="form-auth__title">Сброс пароля</h1>
+        <FormAuth
+          child={
+            <>
+              <label
+                htmlFor="resetPassword"
+                className="form-auth__label"
+              >
+                Новый пароль
+                <input
+                  className=""
+                  type="password"
+                  id="resetPassword"
+                  {...register('resetPassword', {
+                    required: ERR_MESSAGE_REQUIRED,
+                    pattern: {
+                      value: REG_PASSWORD,
+                      message: ERR_MESSAGE_INVALIDPASSWORD
+                    }
+                  })}
+                />
+                <span className="form-auth__err">
+                  {errors?.resetPassword && errors.resetPassword.message}
+                </span>
+              </label>
+            </>
+          }
+          buttonTitle={'Сбросить пароль'}
+          onSubmit={handleSubmit(handleSubmitResetPassword)}
+          err={errors}
+        />
+      </div>
+    );
+  }
 };
