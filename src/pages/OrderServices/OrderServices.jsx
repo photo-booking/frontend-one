1edf960b4d3af9468a56762de9780eb00de8d38c
Evgeniia Semushina <semajenya96@gmail.com>
2023-09-01 13:48:39 +0700

    added header-registration component

--- src/pages/OrderServices/OrderServices.jsx
+++ src/pages/OrderServices/OrderServices.jsx
@@ -1,13 +1,17 @@
 import { useNavigate } from 'react-router-dom';
 import './OrderServices.css';
+import { HeaderRegistration } from '../../components/Header-registration/header-registration';
 
 export const OrderServices = () => {
   const navigate = useNavigate();
 
   return (
-    <div className={'orderServices-container'}>
-      <h1>Отдельный борд с заказами </h1>
-      <button onClick={() => navigate('/catalog')}>В каталог</button>
-    </div>
+    <>
+      <HeaderRegistration></HeaderRegistration>
+      <div className={'orderServices-container'}>
+        <h1>Отдельный борд с заказами </h1>
+        <button onClick={() => navigate('/catalog')}>В каталог</button>
+      </div>
+    </>
   );
 };
