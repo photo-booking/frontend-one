e3188d39aa3d4f605afc551824060514b48426f9
Evgeniia Semushina <semajenya96@gmail.com>
2023-09-01 14:07:40 +0700

    fixed css centered text

--- src/components/Header-start/header-start.jsx
+++ src/components/Header-start/header-start.jsx
@@ -4,7 +4,9 @@ import logo from '../../images/Logo-header.svg'
 export const HeaderStart = () => {
     return (
         <header className='header-start'>
-            <img className='header-start__logo' src={logo}/>
+            <div className='header-start__logo_contsiner'>
+                <img className='header-start__logo' src={logo} />
+            </div>
             <div className='header-start__text'>
                 <a className='header-start__title'>Найти специалиста</a>
                 <a className='header-start__title'>Связаться с нами</a>
