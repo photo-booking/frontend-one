bae70cebc0073531199a0653d3d4ce08bee2d8d9
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-09-02 16:25:44 +0300

    fix base url

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:8080/https://photo-market.acceleratorpracticum.ru/api'; //my adress
+const BASE_URL = 'https://photo-market.acceleratorpracticum.ru/api'; //my adress
 const HEADERS = { 'Content-Type': 'application/json' };
 
 function getResponseData(res) {
