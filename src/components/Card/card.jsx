610308bbaf6940cc362922b2c0ee24313cf09da8
Evgeniia Semushina <semajenya96@gmail.com>
2023-09-26 13:25:28 +0300

    unsuccessful attempt to make a slider

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,8 +1,45 @@
 import { useNavigate } from 'react-router-dom'
 import './card.css';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 
 export const Card = ({user}) => {
+
+    const mediafiles = [
+        {
+            id: "102",
+            author: "Ben Moore",
+            width: 4320,
+            height: 3240,
+            url: "https://unsplash.com/photos/pJILiyPdrXI",
+            download_url: "https://picsum.photos/id/102/4320/3240"
+          },
+          {
+            id: "103",
+            author: "Ilham Rahmansyah",
+            width: 2592,
+            height: 1936,
+            url: "https://unsplash.com/photos/DwTZwZYi9Ww",
+            download_url: "https://picsum.photos/id/103/2592/1936"
+          },
+          {
+            id: "104",
+            author: "Dyaa Eldin",
+            width: 3840,
+            height: 2160,
+            url: "https://unsplash.com/photos/2fl-ocJ5MOA",
+            download_url: "https://picsum.photos/id/104/3840/2160"
+          },
+          {
+            id: "106",
+            author: "Arvee Marie",
+            width: 2592,
+            height: 1728,
+            url: "https://unsplash.com/photos/YnfGtpt2gf4",
+            download_url: "https://picsum.photos/id/106/2592/1728"
+          }
+    ];
 
     const navigate = useNavigate();
     const navigateProfile = (event) => {
@@ -11,14 +48,26 @@ export const Card = ({user}) => {
 
 
     return (
-        <section className="card" onClick={navigateProfile}>
+        <section className="card">
             <div className='card__portfolio'>
-                <img className='card__portfolio_item' src={user.portfolio}/>
+                {/* <Carousel
+                className = "crsl"
+                centerMode
+                interval={1000}
+                infiniteLoop
+                axis = 'horizontal'
+                showThumbs={false}
+        
+                > */}
+                    {/* {mediafiles.map((image) => ( */}
+                        <img className='card__portfolio_item' src={user.portfolio} />
+                    {/* ))} */}
+                {/* </Carousel>                */}
             </div>
 
             <div className='card__about'>
                 <div className='card__about_profile'>
-                    <div className='card__about_info'>
+                    <div className='card__about_info' onClick={navigateProfile}>
                         <p className='card__about_name'>{user.name}</p>
                         <p className='card__about_profession'>{user.profession}</p>
                     </div>
