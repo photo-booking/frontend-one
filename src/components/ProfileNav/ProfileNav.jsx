057b5b1a1cc43b831fc523a1ac6bad6b1277aa3f
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-09-27 20:28:50 +0300

    add navigation

--- src/components/ProfileNav/ProfileNav.jsx
+++ src/components/ProfileNav/ProfileNav.jsx
@@ -4,48 +4,164 @@ import { NavLink } from 'react-router-dom';
 
 export const ProfileNav = props => {
   const { isClient } = props;
+  const [profile, setProfile] = React.useState(false);
+  const [settings, setSettings] = React.useState(false);
+  const [orders, setOrders] = React.useState(false);
+  const [review, setReview] = React.useState(false);
 
-  const navLinkClassName = ({ isActive }) => `profile-nav__link ${isActive ? 'profile-nav__link_active' : ''}`;
+  const navLinkClassName = param => {
+    return `profile-nav__link ${param ? 'profile-nav__link_active' : ''}`;
+  };
   return (
-    <nav className='profile-nav'>
-      <ul className='profile-nav__list'>
-        <li className='profile-nav__item'>
-          <NavLink
-            to="#"
-            className={navLinkClassName}
-          >
-            Профиль
-          </NavLink>
-        </li>
-        <li className='profile-nav__item'>
-          <NavLink
-            to="#"
-            className={navLinkClassName}
-          >
-            Настройки
-          </NavLink>
-        </li>
-        <li className='profile-nav__item'>
-          <NavLink
-            to="#"
-            className={navLinkClassName}
-          >
-            Заказы
-          </NavLink>
-        </li>
-        {!isClient ? (
-          <li className='profile-nav__item'>
+    <>
+      <nav className="profile-nav">
+        <ul className="profile-nav__list">
+          <li className="profile-nav__item">
             <NavLink
               to="#"
-              className={navLinkClassName}
+              className={navLinkClassName(profile)}
+              onClick={() => {
+                setProfile(true);
+                setSettings(false);
+                setOrders(false);
+                setReview(false);
+              }}
             >
-              Отзывы
+              Профиль
             </NavLink>
           </li>
-        ) : (
-          <></>
-        )}
-      </ul>
-    </nav>
+          <li className="profile-nav__item">
+            <NavLink
+              to="#"
+              className={navLinkClassName(settings)}
+              onClick={() => {
+                setProfile(false);
+                setSettings(true);
+                setOrders(false);
+                setReview(false);
+              }}
+            >
+              Настройки
+            </NavLink>
+          </li>
+          <li className="profile-nav__item">
+            <NavLink
+              to="#"
+              className={navLinkClassName(orders)}
+              onClick={() => {
+                setProfile(false);
+                setSettings(false);
+                setOrders(true);
+                setReview(false);
+              }}
+            >
+              Заказы
+            </NavLink>
+          </li>
+          {!isClient ? (
+            <li className="profile-nav__item">
+              <NavLink
+                to="#"
+                className={navLinkClassName(review)}
+                onClick={() => {
+                  setProfile(false);
+                  setSettings(false);
+                  setOrders(false);
+                  setReview(true);
+                }}
+              >
+                Отзывы
+              </NavLink>
+            </li>
+          ) : (
+            <></>
+          )}
+        </ul>
+      </nav>
+      {/* для Профиля  */}
+      {profile ? (
+        <nav>
+          <ul>
+            <li>
+              <NavLink>Личная информация</NavLink>
+            </li>
+            <li>
+              <NavLink>Контакты</NavLink>
+            </li>
+            <li>
+              <NavLink>Портфолио</NavLink>
+            </li>
+            <li>
+              <NavLink>Прайс-лист</NavLink>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <></>
+      )}
+      {/* для Настроек  */}
+      {settings ? (
+        <nav>
+          <ul>
+            <li>
+              <NavLink>Пароль</NavLink>
+            </li>
+            <li>
+              <NavLink>Уведомления</NavLink>
+            </li>
+            <li>
+              <NavLink>Премиум подписка</NavLink>
+            </li>
+            <li>
+              <NavLink>Удалить аккаунт</NavLink>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <></>
+      )}
+      {/* для Заказов  */}
+      {orders ? (
+        <nav>
+          <ul>
+            <li>
+              <NavLink>Заказ 1</NavLink>
+            </li>
+            <li>
+              <NavLink>Заказ 2</NavLink>
+            </li>
+            <li>
+              <NavLink>Заказ 3</NavLink>
+            </li>
+            <li>
+              <NavLink>Заказ 4</NavLink>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <></>
+      )}
+      {/* для Отзывов  */}
+      {review ? (
+        <nav>
+          <ul>
+            <li>
+              <NavLink>Отзыв 1</NavLink>
+            </li>
+            <li>
+              <NavLink>Отзыв 2</NavLink>
+            </li>
+            <li>
+              <NavLink>Отзыв 3</NavLink>
+            </li>
+            <li>
+              <NavLink>Отзыв 4</NavLink>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
