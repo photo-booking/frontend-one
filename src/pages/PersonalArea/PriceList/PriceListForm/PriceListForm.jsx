4c3122a2718c300a3d87f1b95f4c73a7d2bf399c
Stas Tolmachev <stusik@yandex.ru>
2023-10-10 15:35:01 +0300

    fix pricelist

--- src/pages/PersonalArea/PriceList/PriceListForm/PriceListForm.jsx
+++ src/pages/PersonalArea/PriceList/PriceListForm/PriceListForm.jsx
@@ -2,78 +2,94 @@ import './PriceListForm.css';
 import { useState } from 'react';
 
 const PriceListForm = () => {
-    const [cost, setCost] = useState('');
-    const [minTime, setMinTime] = useState('');
-    const [deadline, setDeadline] = useState('');
-  
-    const handleCostChange = (event) => {
-      setCost(event.target.value);
-    }
-  
-    const handleMinTimeChange = (event) => {
-      setMinTime(event.target.value);
-    }
-  
-    const handleDeadlineChange = (event) => {
-      setDeadline(event.target.value);
-    }
+  const [cost, setCost] = useState('');
+  const [minTime, setMinTime] = useState('');
+  const [deadline, setDeadline] = useState('');
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        
-      }
-  
-    return (
-      <form className="pricelist__form" onSubmit={handleSubmit}>
-        <div className="pricelist__form-item">
-          <label htmlFor="cost" className="pricelist__form-label">
-            Стоимость
-          </label>
-          <input
-            type="text"
-            id="cost"
-            className="pricelist__form-input"
-            placeholder="₽/час"
-            value={cost + (cost.length > 0 ? " дней" : "")}
-            onChange={handleCostChange}
-          />
-        </div>
-        <div className="pricelist__form-item">
-          <label htmlFor="min-time" className="pricelist__form-label">
-            Минимальное время
-          </label>
-          <input
-            type="text"
-            id="min-time"
-            className="pricelist__form-input"
-            placeholder="час"
-            value={minTime}
-            onChange={handleMinTimeChange}
-          />
-        </div>
-        <div className="pricelist__form-item">
-          <label htmlFor="deadline" className="pricelist__form-label">
-            Срок сдачи
-          </label>
-          <input
-            type="text"
-            id="deadline"
-            className="pricelist__form-input"
-            placeholder="от, дней"
-            value={deadline}
-            onChange={handleDeadlineChange}
-          />
-        </div>
-        <button
-          type="submit"
-          className="pricelist__form-button"
-          disabled={!cost || !minTime || !deadline}
+  const handleCostChange = event => {
+    event.preventDefault();
+    setCost(event.target.value);
+  };
 
+  const handleMinTimeChange = event => {
+    setMinTime(event.target.value);
+  };
+
+  const handleDeadlineChange = event => {
+    setDeadline(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  return (
+    <form
+      className="pricelist__form"
+      onSubmit={handleSubmit}
+    >
+      <div className="pricelist__form-item">
+        <p className="pricelist__form-description">
+          Фотосъёмка самых важных моментов торжества, таких как церемония, банкет, первый танец и
+          т.д.
+        </p>
+
+        <label
+          htmlFor="cost"
+          className="pricelist__form-label"
         >
-          Сохранить
-        </button>
-      </form>
-    );
+          Стоимость
+        </label>
+        <input
+          type="text"
+          id="cost"
+          className="pricelist__form-input"
+          placeholder="₽/час"
+          value={cost}
+          onChange={handleCostChange}
+        />
+      </div>
+      <div className="pricelist__form-item">
+        <label
+          htmlFor="min-time"
+          className="pricelist__form-label"
+        >
+          Минимальное время
+        </label>
+        <input
+          type="text"
+          id="min-time"
+          className="pricelist__form-input"
+          placeholder="час"
+          value={minTime}
+          onChange={handleMinTimeChange}
+        />
+      </div>
+      <div className="pricelist__form-item">
+        <label
+          htmlFor="deadline"
+          className="pricelist__form-label"
+        >
+          Срок сдачи
+        </label>
+        <input
+          type="text"
+          id="deadline"
+          className="pricelist__form-input"
+          placeholder="от, дней"
+          value={deadline}
+          onChange={handleDeadlineChange}
+        />
+      </div>
+      <button
+        type="submit"
+        className="pricelist__form-button"
+        disabled={!cost || !minTime || !deadline}
+      >
+        Сохранить
+      </button>
+    </form>
+  );
 };
 
 export default PriceListForm;
