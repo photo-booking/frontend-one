56cc385aaca53684778b7722b2d54dabae7d546e
OlgaDavlyud <108143200+OlgaDavlyud@users.noreply.github.com>
2023-10-21 23:38:01 +0300

    add page 404

--- src/pages/404/404.jsx
+++ src/pages/404/404.jsx
@@ -1,12 +1,16 @@
-import { useNavigate } from 'react-router-dom';
 import './404.css';
+import { useNavigate } from 'react-router-dom';
+
 export const Page404 = () => {
   const navigate = useNavigate();
+
   return (
-    <div className={'container'}>
-      <h1>Страница не найдена</h1>
-      <button onClick={() => navigate(`/catalog`)}>Вернуться в каталог</button>
-      <button onClick={() => navigate(`/`)}>На главную страницу</button>
-    </div>
+    <section className='page-error'>
+      <div className='page-error__container'>
+          <span className='page-error__number'>404</span>
+          <h1 className='page-error__title'>Страница не найдена</h1>
+          <button className='page-error__btn' type='button' onClick={() => navigate(-1)}>Вернуться на главную</button>
+      </div>
+    </section>
   );
 };
