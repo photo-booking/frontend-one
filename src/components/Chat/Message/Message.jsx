05735d5bf39fda20f94405c427496d10db74f809
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-12-07 10:03:50 +0300

    update chat messages

--- src/components/Chat/Message/Message.jsx
+++ src/components/Chat/Message/Message.jsx
@@ -1,23 +1,28 @@
 import './Message.css';
 import avatar from '../../../images/Avatar.svg';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { CurrentUserContext } from '../../../components/context/CurrentUserContext';
 
-export const Message = ({ message }) => {
+export const Message = ({ message, currentExpert }) => {
   // console.log(message);
-  const [isExpert, setIsExpert] = useState(false);
-  const messageClassName = `message ${isExpert ? 'message_left' : 'message_right'}`;
-  const messageContainerClassName = `message__container ${
-    isExpert ? 'message__container_left' : 'message__container_right'
-  }`;
-  const messageTextClassName = `message__text ${
-    isExpert ? 'message__text_left' : 'message__text_right'
-  }`;
+  const currentUser = useContext(CurrentUserContext);
+  // console.log(currentExpert);
+  // console.log(currentUser);
+  // console.log(message);
+
+  const messageClassName = `message ${(currentExpert.first_name === message.user) ? 'message_left' : 'message_right'}`;
+  const messageContainerClassName = `message__container ${(currentExpert.first_name === message.user) ? 'message__container_left' : 'message__container_right'
+    }`;
+  const messageTextClassName = `message__text ${(currentExpert.first_name === message.user) ? 'message__text_left' : 'message__text_right'
+    }`;
+
+  const avatarImage = (currentExpert.first_name === message.user) ? (currentExpert.profile_photo ? currentExpert.profile_photo : avatar) : (currentUser.profile_photo ? currentUser.profile_photo : avatar);
 
   return (
     <section className={messageClassName}>
       <img
         className="message__image"
-        src={avatar}
+        src={avatarImage}
         alt="avatar"
       />
       <div className={`${messageContainerClassName} message__container_column`}>
